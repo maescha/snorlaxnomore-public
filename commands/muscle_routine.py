0162624674564e944e3620a74567cb3880e90dac
js <sepiisy@gmail.com>
2025-06-19 15:28:37 -0600

    starting muscle routine

--- commands/muscle_routine.py
+++ commands/muscle_routine.py
@@ -1,2 +1,10 @@
 ## !musc [muscle name]
 ## like `!musc cardiovascular system`
+
+import discord
+import requests
+import json
+
+import random
+
+from utils.workout_routine import _get_random_exercises
